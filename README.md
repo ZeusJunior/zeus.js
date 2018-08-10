0918a183970936b082e1a9533516ef9cc8674a2c
Zeus <zeusjunioryt@gmail.com>
2018-08-10 17:27:49 +0200

    Added heartbeat to bptf

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # zeus.js
 Random stuff for me to use
+
+# Methods
+
+### printMsg
+Will respond with a simple `Module is working` in your console
+
+### heartbeat(usertoken, type)
+- `usertoken` - Your backpack.tf account's access token, which can be found [here](https://backpack.tf/connections)
+- `type` - If set to `"sell"`, marks your sell orders as automatic. If set to `"all"`, marks all your listings as automatic.
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 exports.printMsg = function() {
-  console.log("Message from demo package");
-}
-
-exports.printMsg2 = function() {
-  console.log("Message 2 from demo package");
+  console.log("Module is working");
 }
 
 // test
+
+var request = require('request');
+
+exports.heartbeat = function(usertoken, type) {
+	request.post(
+    'https://backpack.tf/api/aux/heartbeat/v1',
+    { json: 
+        { 
+            token: usertoken, 
+            automatic: type,
+            i_understand_the_risk: 'true',
+        } 
+    },
+        function (error, response, body) {
+            console.log(body);
+        }
+    );
+}
