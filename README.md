5472084fe239986bf40f2f7529cebbabcf48d13c
Zeus <zeusjunioryt@gmail.com>
2018-08-11 23:17:53 +0200

    Improved heartbeat because it sucked

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Random stuff for me to use
 ### printMsg
 Will respond with a simple `Module is working` in your console
 
-### heartbeat(usertoken, type)
-- `usertoken` - Your backpack.tf account's access token, which can be found [here](https://backpack.tf/connections)
+### heartbeat(userToken, type, timeOut)
+- `userToken` - Your backpack.tf account's access token, which can be found [here](https://backpack.tf/connections)
 - `type` - If set to `"sell"`, marks your sell orders as automatic. If set to `"all"`, marks all your listings as automatic.
+- `timeOut` - Timeout in ms, minimum must be 90000 ms to avoid spamming backpack.tf
--- index.js
+++ index.js
@@ -6,12 +6,21 @@ exports.printMsg = function() {
 
 var request = require('request');
 
-exports.heartbeat = function(usertoken, type) {
+exports.heartbeat = function(userToken, type, timeOut) {
+	if (!timeOut) {
+		console.log('timeOut is not set')
+	} else if (isNaN(timeOut)) {
+		console.log('timeOut must be a number')
+	} else if (timeOut < 90000) {
+		console.log('timeOut must be more than 90000ms to avoid spamming backpack.tf')
+	} else {
+	
+	setInterval(function() {
 	request.post(
     'https://backpack.tf/api/aux/heartbeat/v1',
     { json: 
         { 
-            token: usertoken, 
+            token: userToken, 
             automatic: type,
             i_understand_the_risk: 'true',
         } 
@@ -20,4 +29,6 @@ exports.heartbeat = function(usertoken, type) {
             console.log(body);
         }
     );
+}, timeOut);
+	}
 }
